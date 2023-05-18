3bffaeab2d5d116e751ea07b4defe2a928e3402a
obuyajr <felix37brock@gmail.com>
2023-05-18 17:03:54 +0300

    btn

--- src/happychoiceltd/admin_panel.java
+++ src/happychoiceltd/admin_panel.java
@@ -284,9 +284,16 @@ public class admin_panel extends javax.swing.JFrame {
             Class[] types = new Class [] {
                 java.lang.String.class, java.lang.String.class, java.lang.Integer.class
             };
+            boolean[] canEdit = new boolean [] {
+                false, false, false
+            };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
             }
         });
         rooms_display.addMouseListener(new java.awt.event.MouseAdapter() {
