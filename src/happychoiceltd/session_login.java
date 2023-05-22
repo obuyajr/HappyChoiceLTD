62d21072e4640b63b7cfbd4d698a172d1caca2ee
obuyajr <felix37brock@gmail.com>
2023-05-22 15:05:44 +0300

    login

--- src/happychoiceltd/session_login.java
+++ src/happychoiceltd/session_login.java
@@ -3,11 +3,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package happychoiceltd;
+    import java.util.HashMap;
+    import java.util.Map;
+
 
 /**
  *
  * @author felix
  */
 public class session_login {
-    
+ 
+
+    private static final Map<String, Boolean> loggedInUsers = new HashMap<>();
+
+    public static void loginUser(String username) {
+        loggedInUsers.put(username, true);
+    }
+
+    public static void logoutUser(String username) {
+        loggedInUsers.remove(username);
+    }
+
+    public static boolean isUserLoggedIn(String username) {
+        return loggedInUsers.containsKey(username);
+    }
 }
+
+    
